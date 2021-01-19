743421d8ac5bba52aae2a058e58d6417bb456dcb
Himanshu Choudhary <himanshu@wimwisure.com>
2021-01-19 17:47:28 +0530

    Initial commit

---
+++ app/src/main/java/com/himanshu/backgroundtasks/broadcast/BroadcastActivity.kt
@@ -0,0 +1,12 @@
+package com.himanshu.backgroundtasks.broadcast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.himanshu.backgroundtasks.R
+
+class BroadcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_broadcast)
+    }
+}
