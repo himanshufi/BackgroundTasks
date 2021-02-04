13aff5b6c6026b6794cecc47fc33a3d209b640c7
Himanshu Choudhary <himanshu@wimwisure.com>
2021-02-04 11:40:27 +0530

    documentation added

--- app/src/main/java/com/himanshu/backgroundtasks/MainActivity.kt
+++ app/src/main/java/com/himanshu/backgroundtasks/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.himanshu.backgroundtasks
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.himanshu.backgroundtasks.broadcast.BroadcastActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, BroadcastActivity::class.java))
-        finish()
+        /**
+         *
+         * Do read the docs related to the approaches
+         * which are discussed on different branches in
+         * this project, before you start using them.
+         *
+         * Check them here: [Overview]
+         *
+         */
+
+
     }
 }
---
+++ app/src/main/java/com/himanshu/backgroundtasks/Overview.kt
@@ -0,0 +1,28 @@
+@file:Suppress("KDocUnresolvedReference")
+
+package com.himanshu.backgroundtasks
+
+class Overview {
+
+    /**
+     *
+     * List of branches:
+     * 1. master : You are here!!
+     * 2. br_with_wk
+     *
+     */
+
+
+    /**
+     *
+     * [Documentation]
+     *
+     * 1. br_with_wk :
+     *
+     * In this branch, we are executing a background task using `WorkManager` and `AlarmManager`.
+     * The WorkManager is used to fire the AlarmManager, once all the constraints are satisfied.
+     * We are also making the notification snooze for 1 minute on a notification action using the
+     * `Pending Intent` with it.
+     *
+     */
+}
